2712497688807082099ef4aacee24d4d1119c269
Prashanth Ravi <pravi@its-training.com>
2018-12-14 15:13:06 +0000

    Updated API URL From CI

--- src/shared/constants/index.js
+++ src/shared/constants/index.js
@@ -1,3 +1,3 @@
-export const API_DOMAIN = 'https://ev21rh2ijj.execute-api.us-west-2.amazonaws.com/';
+export const API_DOMAIN = 'https://3x7lvj5jk8.execute-api.us-west-2.amazonaws.com/';
 export const API_STAGE_NAME = 'dev';
 export const FIELD_ERROR_MESSAGE = 'This field is required';
