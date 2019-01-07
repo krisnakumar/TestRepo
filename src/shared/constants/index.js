92fe17d4e5f2379ccc7735b1f378683c07c3b8c6
Prashanth Ravi <pravi@its-training.com>
2019-01-07 15:32:49 +0530

    Update Task Request payload

--- src/shared/constants/index.js
+++ src/shared/constants/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-export const API_DOMAIN = 'https://lenxezq4g6.execute-api.us-west-2.amazonaws.com/';
+export const API_DOMAIN = 'https://40ujio8w6a.execute-api.us-west-2.amazonaws.com/';
 export const API_STAGE_NAME = 'dev';
 export const FIELD_ERROR_MESSAGE = 'This field is required';
 export const QUERY_BULIDER_GUIDANCE_MESSAGE = 'Please customize and run your query in the above window. The search result(s) will be displayed here.';
