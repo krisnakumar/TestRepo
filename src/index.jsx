e87d94b8df0b6f50d33554c090cf91b31696348b
Prashanth Ravi <pravi@its-training.com>
2019-01-08 12:29:02 +0530

    Removed Translation Template files

--- src/index.jsx
+++ src/index.jsx
@@ -16,9 +16,6 @@ import { CookiesProvider } from 'react-cookie';
 import App from './containers/_app/App';
 import store from './containers/_app/store';
 import ScrollToTop from './containers/_app/ScrollToTop';
-import { config as i18nextConfig } from './translations/index';
-
-i18next.init(i18nextConfig);
 
 render(
   <Provider store={store}>
