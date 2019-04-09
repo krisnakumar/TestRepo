c4efa7dd46163eaec9dd9f5d3a748ff573aabf6c
Shoba Eswar <seswar@its-training.com>
2019-04-09 23:32:20 +0530

    Enhanced Task API

--- ReportBuilderAPI/ReportBuilderAPI/Repository/TaskRepository.cs
+++ ReportBuilderAPI/ReportBuilderAPI/Repository/TaskRepository.cs
@@ -352,25 +352,25 @@ namespace ReportBuilderAPI.Repository
                                         }
                                     }
 
-                                    else if (queryBuilderRequest.ColumnList.Contains(Constants.ASSIGNED_COMPANY_QUALIFICATION))
-                                    {
-                                        TaskModel task = taskList.Where(x => x.UserId == taskModel.UserId).Select(x => x).FirstOrDefault();
-                                        if (task != null)
-                                        {
-                                            if (taskModel.RoleStatus.ToUpper() == Constants.QUALIFIED)
-                                            {
-                                                task.CompletedUserQualification = taskModel.CompletedUserQualification;
-                                            }
-                                            else
-                                            {
-                                                task.IncompleteUserQualification = taskModel.IncompleteUserQualification;
-                                            }
-                                        }
-                                        else
-                                        {
-                                            taskList.Add(taskModel);
-                                        }
-                                    }
+                                    //else if (queryBuilderRequest.ColumnList.Contains(Constants.ASSIGNED_COMPANY_QUALIFICATION))
+                                    //{
+                                    //    TaskModel task = taskList.Where(x => x.UserId == taskModel.UserId).Select(x => x).FirstOrDefault();
+                                    //    if (task != null)
+                                    //    {
+                                    //        if (taskModel.RoleStatus.ToUpper() == Constants.QUALIFIED)
+                                    //        {
+                                    //            task.CompletedUserQualification = taskModel.CompletedUserQualification;
+                                    //        }
+                                    //        else
+                                    //        {
+                                    //            task.IncompleteUserQualification = taskModel.IncompleteUserQualification;
+                                    //        }
+                                    //    }
+                                    //    else
+                                    //    {
+                                    //        taskList.Add(taskModel);
+                                    //    }
+                                    //}
                                     else
                                     {
                                         taskList.Add(taskModel);
